ffcd50db7d468293d885ecf45d7b6c420888074a
unknown <ekeleolu1@gmail.com>
2025-08-04 08:57:38 +0100

    first ejs project commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 5020;
+
+app.use(express.urlencoded({ extended: false }));
+
+app.set('view engine', 'ejs');
+const middleware = (req, res, next) => {
+  console.log(`Request URL: ${req.url}`);
+  next();
+}
+
+app.use(middleware);
+app.get('/', (req, res) => {
+  res.render('homepage', { title: 'Home Page' });
+})
+app.post('/submit', (req, res) => {
+  const { name, email, password } = req.body;
+  console.log(`Name: ${name}, Email: ${email}, Password: ${password}`);
+  console.log('Form submitted successfully!');
+  res.render('submit', { name, email });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+}); 
